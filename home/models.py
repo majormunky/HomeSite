9ffcb4d7e77a67980df8e445b35b8934756a7b0e
Josh Bright <josh.bright@gmail.com>
2020-07-30 17:50:19 -0700

    Added some models to deal with blog posts

--- home/models.py
+++ home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class BlogCategory(models.Model):
+	name = models.CharField(max_length=64)
+	slug = models.SlugField(max_length=64)
+
+
+class BlogPost(models.Model):
+	title = models.CharField(max_length=256)
+	slug = models.SlugField(max_length=256)
+	body = models.TextField()
+	created = models.DateTimeField(default=timezone.now)
+	updated = models.DateTimeField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	category = models.ForeignKey(
+		BlogCategory,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
+
+
+class BlogImage(models.Model):
+	image = models.ImageField(upload_to="blog_posts/")
+	post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
